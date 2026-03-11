d7ecf829746c0bdf12ed62f3510668cf31ea58ec
carvilsi <carvilsi.mail.list@gmail.com>
2026-03-11 10:33:18 +0100

    :white_check_mark: tests wip

---
+++ tests/expected_outputs/expected_default_delay_out.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# parsed onto ydotool commands by flipper0badusb_test with <3 by (#4|2
+# https://github.com/carvilsi/flipper0-badUSB-linux-tester
+# trying stuff
+# Open Terminal app
+ydotool key 125:1 30:1 30:0 125:0
+ydotool type 'Terminal'
+sleep 5
+ydotool key 28:1 28:0
+# Wait 10 seconds after all commands
+sleep 10
+# echo something 
+sleep 10
+ydotool type 'echo "The world is all that is the case"'
+sleep 10
+ydotool key 28:1 28:0
+sleep 10
+ydotool type 'echo "The world is the totality of facts, not of things"'
+ydotool key 28:1 28:0
+sleep 10
+# hide the Terminal app
+sleep 10
+ydotool key 125:1 35:1 35:0 125:0
+sleep 10
